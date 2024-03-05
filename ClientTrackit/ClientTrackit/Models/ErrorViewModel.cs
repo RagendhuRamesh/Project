86ad723046a21d74306a21d0f30faf2d4223fbd6
RagendhuRamesh <ragendhuram@gmail.com>
2024-03-05 10:43:35 +0530

    initial commit

---
+++ ClientTrackit/ClientTrackit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClientTrackit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
